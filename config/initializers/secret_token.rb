10be9555d9d88886637964065eb05229d9b06b9a
Michael Fashola <michaelfashola@gmail.com>
2014-05-03 21:14:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChurchApp::Application.config.secret_key_base = 'c8d850d2f3bbd2ae570e97d9271c9bf46009d0979629d7f124e2d6bfd5456fb55b2ba5bdefcde878409b1a1eff4dd280e539ca240008b0dd21f4b4f7b1c515d8'
