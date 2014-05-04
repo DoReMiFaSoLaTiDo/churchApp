8f48df4148f17d77d660ae1a69a7e1e95c67cea0
Michael Fashola <michaelfashola@gmail.com>
2014-05-04 18:23:26 -0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ChurchApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/ministries"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
